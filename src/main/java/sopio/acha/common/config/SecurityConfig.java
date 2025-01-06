1b29c97271a4358fdb5f404bcde161d3c64500c6
kwj0175 <kwj075@gmail.com>
2025-01-06 14:35:02 +0900

    MOD: BCryptPasswordEncoder 빈 등록, LoginFilter 필터 추가, PERMET_ALL_PATTERNS member 엔드포인트 추가

--- src/main/java/sopio/acha/common/config/SecurityConfig.java
+++ src/main/java/sopio/acha/common/config/SecurityConfig.java
@@ -4,22 +4,42 @@ import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import lombok.RequiredArgsConstructor;
+import sopio.acha.jwt.JWTCreator;
+import sopio.acha.jwt.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+	private final AuthenticationConfiguration authenticationConfiguration;
+
+	private final JWTCreator jwtCreator;
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+		return configuration.getAuthenticationManager();
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,6 +49,7 @@ public class SecurityConfig {
 			.httpBasic(AbstractHttpConfigurer::disable)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.logout(AbstractHttpConfigurer::disable)
+			.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtCreator), UsernamePasswordAuthenticationFilter.class)
 			.sessionManagement(session -> session
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			)
@@ -54,7 +75,8 @@ public class SecurityConfig {
 	};
 
 	private static final String[] PERMIT_ALL_PATTERNS = {
-		"/api/v1/"
+		"/api/v1/",
+		"/member/**"
 	};
 
 	CorsConfigurationSource corsConfigurationSource() {
