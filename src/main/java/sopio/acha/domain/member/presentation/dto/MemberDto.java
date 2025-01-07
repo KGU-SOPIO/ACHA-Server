597e4351dce372f138e9e5326b4a286e0e9d6b87
kwj0175 <kwj075@gmail.com>
2025-01-07 13:14:34 +0900

    MOD: MemberDto Schema 변경

--- src/main/java/sopio/acha/domain/member/presentation/dto/MemberDto.java
+++ src/main/java/sopio/acha/domain/member/presentation/dto/MemberDto.java
@@ -19,10 +19,10 @@ public class MemberDto {
     @Schema(description = "이름", example = "권우진")
     private String name;
 
-    @Schema(description = "대학", example = "경기대학교")
+    @Schema(description = "단과대", example = "소프트웨어경영대학")
     private String college;
 
-    @Schema(description = "단과대", example = "소프트웨어경영대학")
+    @Schema(description = "학부", example = "컴퓨터공학부")
     private String department;
 
     @Schema(description = "학과", example = "컴퓨터공학과")
