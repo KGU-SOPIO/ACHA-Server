ea291a2fcea6f3386425b17b3761589208258bc2
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-02-04 15:14:52 +0900

    feat: 내 강좌 목록 API 구현

--- src/main/java/sopio/acha/domain/lecture/domain/Lecture.java
+++ src/main/java/sopio/acha/domain/lecture/domain/Lecture.java
@@ -1,18 +1,25 @@
 package sopio.acha.domain.lecture.domain;
 
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import org.hibernate.annotations.Formula;
+
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import sopio.acha.common.domain.BaseTimeEntity;
 import sopio.acha.common.exception.ExtractorErrorException;
 import sopio.acha.domain.member.domain.Member;
@@ -33,7 +40,16 @@ public class Lecture extends BaseTimeEntity {
 
 	private String lectureRoom;
 
-	private String day;
+	@Enumerated(STRING)
+	private LectureDay day;
+
+	@Formula("CASE WHEN day = '월요일' THEN 1 " +
+		"WHEN day = '화요일' THEN 2 " +
+		"WHEN day = '수요일' THEN 3 " +
+		"WHEN day = '목요일' THEN 4 " +
+		"WHEN day = '금요일' THEN 5 " +
+		"ELSE 0 END")
+	private int dayOrder;
 
 	private int classTime;
 
@@ -56,7 +72,7 @@ public class Lecture extends BaseTimeEntity {
 					lecture.identifier = (String) map.get("identifier");
 					lecture.professor = (String) map.get("professor");
 					lecture.lectureRoom = (String) map.get("lectureRoom");
-					lecture.day = (String) map.get("day");
+					lecture.day = LectureDay.valueOf((String) map.get("day"));
 					lecture.classTime = Optional.ofNullable((Integer) map.get("classTime")).orElse(0);
 					lecture.startAt = Optional.ofNullable((Integer) map.get("startAt")).orElse(0);
 					lecture.endAt = Optional.ofNullable((Integer) map.get("endAt")).orElse(0);
--- src/main/java/sopio/acha/domain/lecture/infrastructure/LectureRepository.java
+++ src/main/java/sopio/acha/domain/lecture/infrastructure/LectureRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import sopio.acha.domain.lecture.domain.Lecture;
+import sopio.acha.domain.lecture.domain.LectureDay;
 
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-	List<Lecture> findAllByMemberIdAndDayAndIsPresentTrue(String memberId, String day);
+	List<Lecture> findAllByMemberIdAndDayAndIsPresentTrueOrderByStartAtAsc(String memberId, LectureDay day);
+
 	Boolean existsByMemberId(String memberId);
+
+	List<Lecture> findAllByMemberIdOrderByDayOrderAscStartAtAsc(String memberId);
+
 }
