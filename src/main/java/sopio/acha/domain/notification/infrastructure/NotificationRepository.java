88a902c1637b5aebd68617123dfdf5ded2b8a5ec
leehaneum <leehaneum160924@kyonggi.ac.kr>
2025-03-15 01:41:23 +0900

    feat: 공지 개별 조회 API

--- src/main/java/sopio/acha/domain/notification/infrastructure/NotificationRepository.java
+++ src/main/java/sopio/acha/domain/notification/infrastructure/NotificationRepository.java
@@ -1,6 +1,7 @@
 package sopio.acha.domain.notification.infrastructure;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,7 @@ import sopio.acha.domain.notification.domain.Notification;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 	boolean existsByIndexAndLectureId(int index, Long lectureId);
 
-	List<Notification> findAllByLectureCode(String code);
+	Optional<Notification> findByIndexAndLectureId(int index, Long lectureId);
+
+	List<Notification> findAllByLectureCodeOrderByIndexDesc(String code);
 }
--- src/main/java/sopio/acha/domain/notification/presentation/NotificationController.java
+++ src/main/java/sopio/acha/domain/notification/presentation/NotificationController.java
@@ -2,6 +2,7 @@ package sopio.acha.domain.notification.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import sopio.acha.domain.notification.application.NotificationService;
+import sopio.acha.domain.notification.presentation.response.NotificationDetailResponse;
 import sopio.acha.domain.notification.presentation.response.NotificationListResponse;
 
 @RestController
@@ -21,10 +23,19 @@ public class NotificationController {
 
 	@GetMapping
 	@Operation(summary = "강좌별 공지사항 목록 조회 API",description = "강좌별 공지사항 목록을 불러옵니다")
-	public ResponseEntity<NotificationListResponse> notifications(
+	public ResponseEntity<NotificationListResponse> getNotifications(
 		@RequestParam String code
 	) {
 		NotificationListResponse response = notificationService.getNotifications(code);
+		return ResponseEntity.ok(response);
+	}
+
+	@GetMapping("/{id}")
+	@Operation(summary = "공지사항 조회 API",description = "공지사항을 불러옵니다")
+	public ResponseEntity<NotificationDetailResponse> getNotificationDetail(
+		@PathVariable Long id
+	) {
+		NotificationDetailResponse response = notificationService.getNotificationDetail(id);
 		return ResponseEntity.ok(response);
 	}
 }
