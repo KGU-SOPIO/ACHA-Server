ccff6b04a31f2ebd9eed09385ddddd923eac2781
kwj0175 <kwj075@gmail.com>
2025-03-28 00:58:32 +0900

    feat : 빌더 삭제 및 of메서드로 변경

---
+++ src/main/java/sopio/acha/domain/fcm/presentation/request/AlertRequest.java
@@ -0,0 +1,12 @@
+package sopio.acha.domain.fcm.presentation.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AlertRequest(
+    @Schema(description = "알림 상태")
+    boolean status
+) {
+    public static AlertRequest of(Boolean alert) {
+        return new AlertRequest(alert);
+    }
+}
