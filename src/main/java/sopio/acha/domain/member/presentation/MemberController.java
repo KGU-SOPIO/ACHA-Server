0d2ce4e642e8bbd60967e697c2c83159546cc246
kwj0175 <kwj075@gmail.com>
2025-04-01 18:09:42 +0900

    feat : /data -> /student-data 엔드포인트 변경

--- src/main/java/sopio/acha/domain/member/presentation/MemberController.java
+++ src/main/java/sopio/acha/domain/member/presentation/MemberController.java
@@ -52,7 +52,7 @@ public class MemberController {
         return ResponseEntity.ok().body(response);
     }
 
-    @PostMapping("/data")
+    @PostMapping("/student-data")
     @Operation(summary = "신규 회원 LMS 학생 정보 추출 API", description = "신규 회원 가입 시에 LMS에서 학생 정보를 추출합니다.")
     public ResponseEntity<MemberSummaryResponse> getNewMemberDataFromLMS(
         @RequestBody MemberRequest request
