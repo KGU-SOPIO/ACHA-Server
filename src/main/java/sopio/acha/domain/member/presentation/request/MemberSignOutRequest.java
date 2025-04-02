0f4c2d40508fd50b8b9dd03d6b232ad5bc8a9a90
kwj0175 <kwj075@gmail.com>
2025-04-02 20:29:32 +0900

    feat : MemberSignOutRequest deviceToken 삭제

--- src/main/java/sopio/acha/domain/member/presentation/request/MemberSignOutRequest.java
+++ src/main/java/sopio/acha/domain/member/presentation/request/MemberSignOutRequest.java
@@ -1,15 +1,11 @@
 package sopio.acha.domain.member.presentation.request;
 
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MemberSignOutRequest(
 	@Schema(description = "비밀번호", requiredMode = REQUIRED)
-	String password,
-
-	@Schema(description = "디바이스 토큰", requiredMode = NOT_REQUIRED)
-	String deviceToken
+	String password
 ) {
 }
